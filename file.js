62964f725a739451f892631e87bd0701cadfc4ed
11klassproj <160300541+11klassproj@users.noreply.github.com>
2024-03-05 10:06:47 +1000

    Update file.js

--- file.js
+++ file.js
@@ -13,7 +13,7 @@ function myClick2() {
     console.log(b);
     let t = ((Math.sin(ar))*2*b)/9.80665;
     document.getElementById("t").innerHTML = t.toFixed(3);
-    let h = b*b*Math.sin(ar)*Math.sin(ar)/9.80665*2;
+    let h = b*b*((Math.sin(ar))*Math.sin(ar))/9.80665*2;
     document.getElementById("h").innerHTML = h.toFixed(3);
     let l = b*b*2*Math.sin(ar)*Math.cos(ar)/9.80665;
     document.getElementById("l").innerHTML = l.toFixed(3);
